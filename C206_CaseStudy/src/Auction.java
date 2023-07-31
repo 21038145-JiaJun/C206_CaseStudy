948b271e468831ca07e4365c4ebc195b522bd4d0
Ivan <Ivan@10.175.6.139>
2023-07-31 17:05:21 +0800

    Created Auction class

---
+++ C206_CaseStudy/src/Auction.java
@@ -0,0 +1,32 @@
+
+public class Auction {
+	
+	private int auctionID;
+	private String description;
+	private double currentBid;
+	
+	public Auction(int auctionID, String description, double currentBid) {
+		super();
+		this.auctionID = auctionID;
+		this.description = description;
+		this.currentBid = currentBid;
+	}
+
+	public double getCurrentBid() {
+		return currentBid;
+	}
+
+	public void setCurrentBid(double currentBid) {
+		this.currentBid = currentBid;
+	}
+
+	public int getAuctionID() {
+		return auctionID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	
+}
