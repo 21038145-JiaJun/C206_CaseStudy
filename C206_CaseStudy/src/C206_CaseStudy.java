f7e3185b10c5dfce7ba11cb921df805605b17f07
lumji <lumji@JJ>
2023-07-31 20:24:03 +0800

    Added base code for addBid

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,45 +1,58 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+
+	private static final int OPTION_QUIT = 6;
+	private static final int OPTION_USER = 1;
+	private static final int OPTION_AUCTION = 2;
+	private static final int OPTION_ITEM = 3;
+	private static final int OPTION_BID = 4;
+	private static final int OPTION_PAYMENT = 5;
+
+	private static final int ADD = 1;
+	private static final int VIEW_ALL = 2;
+	private static final int DELETE = 3;
+	private static final int EXIT = 4;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch();
 
 	}
-	
+
 	private static void launch() {
+
+		ArrayList<Item> itemList = new ArrayList<Item>();
+
 		int option = -99;
-		
+
 		while (option != 6) {
 			mainMenu();
-			
+
 			option = Helper.readInt("Enter an option > ");
-			
-			if (option == 1) {
+
+			if (option == OPTION_USER) {
 				user();
-			} else if (option == 2) {
-				
-			} else if (option == 3) {
-				
-			} else if (option == 4) {
-				
-			} else if (option == 5) {
-				
-			} else if (option == 6) {
-				
+			} else if (option == OPTION_AUCTION) {
+
+			} else if (option == OPTION_ITEM) {
+
+			} else if (option == OPTION_BID) {
+				bid();
+			} else if (option == OPTION_PAYMENT) {
+
+			} else if (option == OPTION_QUIT) {
+
 			} else {
 				System.out.println("Invalid option");
 			}
-			
+
 		}
 	}
-	
-	//Display the main menu options
+
+	// Display the main menu options
 	private static void mainMenu() {
-		Helper.line(80, "=");
-		System.out.println("CAMPUS ONLINE AUCTION SHOP (COAS)");
-		Helper.line(80, "=");
+		C206_CaseStudy.setHeader("CAMPUS ONLINE AUCTION SHOP (COAS)");
 		System.out.println("1. Maintain Users");
 		System.out.println("2. Maintain Auctions");
 		System.out.println("3. Maintain Items");
@@ -47,26 +60,72 @@ public class C206_CaseStudy {
 		System.out.println("5. Maintain Payments");
 		System.out.println("6. Exit");
 	}
-	
-	//================================= Option 1: Users (Cedric) =================================
+
+	// This is to set header for the menus
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+
+	// ================================= Option 1: Users (Cedric)
+	// =================================
 	private static void user() {
-		int option = -99; 
+		int option = -99;
 		userMenu();
-		while (option != 1) {
-			
+		while (option != EXIT) {
+
 		}
 	}
-	
+
 	private static void userMenu() {
-		Helper.line(80, "=");
-		System.out.println("USER MENU");
-		Helper.line(80, "=");
+		C206_CaseStudy.setHeader("USER MENU");
 		System.out.println("1. Add a new user");
 		System.out.println("2. View All Users");
 		System.out.println("3. Delete an existing user");
 		System.out.println("4. Exit");
 	}
+
+	// ================================= Option 4: Bids (Jia Jun)
+	// =================================
+
+	private static void bidMenu() {
+		C206_CaseStudy.setHeader("BID MENU");
+		System.out.println("1. Add a New Bid");
+		System.out.println("2. View All Bids");
+		System.out.println("3. Delete an Existing Bid");
+		System.out.println("4. Exit");
+	}
+
+	private static void bid() {
+		int option = -99;
+		bidMenu();
+		while (option != EXIT) {
+			if (option == ADD) {
+				
+			} else if (option == VIEW_ALL) {
+				
+			} else if (option == DELETE) {
+				
+			}
+		}
+	}
 	
-	//================================= Option 1-6: [Function] ([Name]) =================================
-	
+
+	private static void addBid(ArrayList<Item> itemList, Item it) {
+		Item item;
+		for (int i = 0; i < itemList.size(); i++) {
+			item = itemList.get(i);
+			if (item.getAssetTag().equalsIgnoreCase(item.getAssetTag()))
+				return;
+		}
+		if ((it.getAssetTag().isEmpty()) || (it.getDescription().isEmpty())) {
+			return;
+		}
+
+		itemList.add(it);
+	}
+	// ================================= Option 1-6: [Function] ([Name])
+	// =================================
+
 }
