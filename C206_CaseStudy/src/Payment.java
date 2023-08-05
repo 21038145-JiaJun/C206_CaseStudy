435f4feb1769e93e37e53cfc4deee05ab455dc1f
Ivan <Ivan@DESKTOP-6PFN3VO>
2023-08-05 22:24:52 +0800

    Added new field paymentID in Payment class

--- C206_CaseStudy/src/Payment.java
+++ C206_CaseStudy/src/Payment.java
@@ -1,12 +1,14 @@
 
 public class Payment {
 	
+	private int paymentID;
 	private Auction auction;
 	private Item item;
 	private boolean paid;
 	
 	
-	public Payment(Auction auction, Item item, boolean paid) {
+	public Payment(int paymentID, Auction auction, Item item, boolean paid) {
+		this.paymentID = paymentID;
 		this.auction = auction;
 		this.item = item;
 		this.paid = paid;
@@ -18,6 +20,10 @@ public class Payment {
 		} else {
 			return "No";
 		}
+	}
+
+	public int getPaymentID() {
+		return paymentID;
 	}
 
 	public Auction getAuction() {
