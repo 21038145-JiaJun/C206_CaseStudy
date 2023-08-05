13e7e7ee0de96b64688b0113469e240d6e07a007
Ivan <Ivan@DESKTOP-6PFN3VO>
2023-08-05 19:25:18 +0800

    Added constructor, setters and getters to Payment class

--- C206_CaseStudy/src/Payment.java
+++ C206_CaseStudy/src/Payment.java
@@ -1,8 +1,33 @@
 
 public class Payment {
 	
-	private int itemID;
-	private double price;
+	private Auction auction;
+	private Item item;
+	private boolean paid;
+	
+	
+	public Payment(Auction auction, Item item, boolean paid) {
+		this.auction = auction;
+		this.item = item;
+		this.paid = paid;
+	}
+
+	public String isPaid() {
+		if (paid == true) {
+			return "Yes";
+		} else {
+			return "No";
+		}
+	}
+
+	public Auction getAuction() {
+		return auction;
+	}
+
+
+	public Item getItem() {
+		return item;
+	}
 	
 	
 }
